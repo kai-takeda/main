9ac1ffba187a75094a8a2ee7e80b6896807f3c9b
kai-takeda <takeda179j3096@gmail.com>
2023-05-09 22:54:04 +0900

    課題1.23が完了しました。メインクラスTheme.java,サブクラスTheme1.javaです。新規作成です。

---
+++ java_test/src/lesson_23/Theme.java
@@ -0,0 +1,26 @@
+package lesson_23;
+
+public class Theme {
+
+	/*
+ 	
+ 	下記がコンソールに出力されるように作成してください
+ 	※thisとsetterとgetterとフィールドを使ってください
+ 	
+  	動物名：ライオン
+	体長：2.1m
+	速度：80km/h
+ 	
+ */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// インスタンスを作成、その後表示
+		Theme1 theme1 = new Theme1("ライオン", 2.1, 80) ;
+		System.out.println("動物名：" + theme1.getName());
+		System.out.println("体長：" + theme1.getHeight() + "m");
+		System.out.println("速度：" + theme1.getFast() + "km/h");
+		
+
+	}
+
+}
---
+++ java_test/src/lesson_23/Theme1.java
@@ -0,0 +1,47 @@
+package lesson_23;
+
+public class Theme1 {
+	
+/*
+ 	
+ 	下記がコンソールに出力されるように作成してください
+ 	※thisとsetterとgetterとフィールドを使ってください
+ 	
+  	動物名：ライオン
+	体長：2.1m
+	速度：80km/h
+ 	
+ */
+	// 別クラスから参照されないようにprivateで設定
+	private String name;
+	private double height;
+	private int fast;
+	
+	// コンストラクタを作成
+	public Theme1(String name, double height, int fast) {
+		this.name = name;
+		this.height = height;
+		this.fast = fast;
+	}
+	// それぞれのセッターを設定
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public void setFastt(double fastt) {
+		this.fast = fast;
+	}
+	// それぞれのゲッターを設定。
+	public String getName() {
+		return this.name;
+	}
+	public double getHeight() {
+		return this.height;
+	}
+	public int getFast() {
+		return this.fast;
+	}
+	
+}
